6ab0b3b13ce4a1c8aa740cd277ae9c86b729c920
Praveen Bhati <127674992+Praveen9719@users.noreply.github.com>
2025-09-21 21:00:11 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,26 +1,30 @@
+// ✅ backend/index.js
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-
 import path from "path";
 
 import { connectDB } from "./src/lib/db.js";
-
 import authRoutes from "./src/routes/auth.route.js";
 import messageRoutes from "./src/routes/message.route.js";
 import { app, server } from "./src/lib/socket.js";
 
 dotenv.config();
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5001;
 const __dirname = path.resolve();
+
+const allowedOrigins = [
+  "http://localhost:5173",                    // ✅ Development
+  "https://chatapp-frontend-nwib.onrender.com"// ✅ Production      //"http://localhost:5173",         // Update this to your frontend 
+];
 
 app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin:"https://chatapp-frontend-nwib.onrender.com",                      //"http://localhost:5173",         // Update this to your frontend URL
+    origin: allowedOrigins,
     credentials: true,
   })
 );
@@ -30,7 +34,6 @@ app.use("/api/messages", messageRoutes);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
-
   app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
@@ -40,3 +43,5 @@ server.listen(PORT, () => {
   console.log("server is running on PORT:" + PORT);
   connectDB();
 });
+
+                                        
