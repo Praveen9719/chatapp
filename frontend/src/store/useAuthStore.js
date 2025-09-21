6541ebf6640169a26e33a89c0cdb10626388313f
Praveen Bhati <127674992+Praveen9719@users.noreply.github.com>
2025-09-21 20:15:55 +0530

    Update useAuthStore.js

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -6,7 +6,7 @@ import { io } from "socket.io-client";
 const SOCKET_URL = 
   import.meta.env.MODE === "development" 
   ? "http://localhost:5001" 
-  : "https://chatapp-backend-x0w0.onrender.com";   // backend url
+  : "https://chatapp-backend-x0w0.onrender.com";  // backend url
 
 export const useAuthStore = create((set, get) => ({
   authUser: null,
